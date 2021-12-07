b440376926d7aaaa5893965f859eaddc48aa52b8
BeomChu <izekfzha@gmail.com>
2021-12-07 15:55:34 +0900

    test commit
    테스트 범수 수정
    범수바보 필드 생성, 연습메서드 2 작성

--- src/main/java/com/example/demo/controller/TestBeomsu.java
+++ src/main/java/com/example/demo/controller/TestBeomsu.java
@@ -4,7 +4,14 @@ public class TestBeomsu {
 
     private String beomsu;
 
+    private String beomsuBabo;
+
+
     public void practice(){
         System.out.println(beomsu);
+    }
+
+    public void practice2(){
+        System.out.println(beomsuBabo);
     }
 }
